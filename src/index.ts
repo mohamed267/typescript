8927206baf7eff7e10cb8ab29cced81178a1bcf0
mohamed267 <m.rabahsidhoum@esi-sba.dz>
2023-07-04 22:05:40 +0100

    creating json type add json formating check

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,27 @@
-/**
- * create a promise thet resolved after some time
- * @param n number of milisecods before promise resolves
- * 
- */
+type Primitive = string | number | boolean | null
 
-function  timeout(n: number){
-    return new Promise((res)=> setTimeout(res, n))
-}
+type JSONObject  = { [k:string]: JSONValue }
+type JSONArray =   JSONValue[]
+type JSONValue = Primitive | JSONArray | JSONObject
 
+
+function isJSON(arg: JSONValue){}
 
 
 
+isJSON("hello")
 
-/**
- * Add thre  number
- * @param a firest number
- * @param b second number
- */
-export async function addNumbers(a: number , b: number){
-    await timeout(500)
-    return a +b
-}
 
-(async()=>{
-    console.log(await addNumbers(3, 4))
-})
+isJSON([3, 3, 5, 5])
+isJSON(false)
+isJSON(true)
+isJSON(4)
+isJSON({a : { f :4}})
+
+isJSON(()=>{})
 
 
 
-cot 
+
+
+
